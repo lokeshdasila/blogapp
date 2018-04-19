fe23f6d9031382347952ff783f6b1644a6ef219f
lokeshdasila <lokeshdasila23@gmail.com>
2018-04-19 06:47:33 +0530

    till new route b4 show route

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 var blogSchema = new mongu.Schema({
     title   : String,
     image   : String,
-    body    : String,
+    content : String,
     created : {type : Date,default : Date.now}
 });
 
@@ -25,15 +25,49 @@ var blogModel = mongu.model("blog",blogSchema);
 // blogModel.create({
 //     title   : "First of many!!!",
 //     image   : "/y0.jpeg",
-//     body    : "This is my first blog and start of something awesome!!! Hope to do something good and go a long way ;)"
+//     content : "This is my first blog and start of something awesome!!! Hope to do something good and go a long way ;)"
 // });
 
 // ROUTES
-
+// root
+app.get("/",(req,res)=>{
+    console.log("Get : root");
+    res.redirect("/blogs");
+})
 // index
 app.get("/blogs",(req,res)=>{
-    res.render("index");
+    console.log("Get : /blogs");
+    blogModel.find({},(err , blogs)=>{
+        if(err){
+            res.send("<h1>Oops an error<h2>");
+        }
+        else{
+            res.render("index",{pageBlogs : blogs});
+        }
+    })
+    
 });
+
+// new
+app.get("/blogs/new",(req,res)=>{
+    console.log("Get : /blogs/new");
+    res.render("new.ejs");
+});
+
+//Create
+app.post("/blogs",(req,res)=>{
+    // create new blog
+    blogModel.create(req.body.blog,(err,newBlog)=>{
+        if(err){
+            res.render("/blogs/new");
+        }
+        else{
+            res.redirect("/blogs");
+        }
+    });
+    // redirects to blogs
+    //res.redirect("/blogs");
+})
 
 app.listen(port,ip,()=>{
     console.log("Server started in port "+port+" and ip "+ip);
